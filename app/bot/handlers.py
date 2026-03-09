4dbd57ec500ad230cc3f04b7cb7d1791c113d7bb
Sanzhar <ssoltybekov@gmail.com>
2026-03-09 22:07:55 +0500

    new handlers

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -6,6 +6,7 @@ from app.bot.states import CourseForm
 from app.calculator.gpa import calculate_gpa
 from app.model.course import Course
 from app.bot.keyboards import credits_keyboard, grades_keyboard, choice_keyboard
+from aiogram import F
 
 router = Router()
 
@@ -36,10 +37,19 @@ async def save_grade(message: Message, state: FSMContext):
     await state.update_data(courses=courses)
     await state.set_state(CourseForm.waiting_choice)
     await message.answer("Do you want to add more courses or evaluate GPA?", reply_markup=choice_keyboard())
-    
-    # result = calculate_gpa([new_course])
-    # text = (
-    #     f"That is your GPA - {result}"
-    # )
-    # await message.answer(text)
+
+@router.message(CourseForm.waiting_choice, F.text == "Add new course")
+async def add_more(message: Message, state: FSMContext):
+    await state.set_state(CourseForm.waiting_name)
+    await message.answer("Name of the course...")
+
+@router.message(CourseForm.waiting_choice, F.text == "Evaluate the GPA")
+async def evaluate(message: Message, state: FSMContext):
+    data = await state.get_data()
+    courses = data.get("courses", [])
+    result = calculate_gpa(courses)
+    text = (
+        f"That is your GPA - {result}"
+    )
+    await message.answer(text)
 
